379681da57d2195262c9b98e5d1d7fc1bd765002
minturtle <minseok0425@kakao.com>
2024-09-16 00:01:07 +0900

    refactor: System Message 외부 설정파일로 분리

--- src/main/java/org/minturtle/careersupport/interview/service/InterviewService.java
+++ src/main/java/org/minturtle/careersupport/interview/service/InterviewService.java
@@ -2,6 +2,7 @@ package org.minturtle.careersupport.interview.service;
 
 
 import org.minturtle.careersupport.common.service.ChatService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -11,36 +12,23 @@ import java.util.List;
 public class InterviewService {
 
     private final ChatService chatService;
-
-    public InterviewService(ChatService chatService) {
+    private final String interviewSystemMessage;
+    private final String followSystemMessage;
+    public InterviewService(
+            ChatService chatService,
+            @Value("${spring.ai.openai.messages.interview-system-message}") String interviewSystemMessage,
+            @Value("${spring.ai.openai.messages.follow-system-message}") String followSystemMessage
+        ) {
         this.chatService = chatService;
+        this.interviewSystemMessage = interviewSystemMessage;
+        this.followSystemMessage = followSystemMessage;
     }
 
-
-
     public Flux<String> getInterviewQuestion(String theme){
-        String systemMessage = """
-        당신은 AI 면접관입니다. 역할은 다음과 같습니다:
-        1. 사용자가 입력한 주제에 맞는 면접 질문을 하나씩 생성합니다.
-        2. 사용자가 질문에 답변을 하면, 그 답변을 평가하고 점수를 부여합니다.
-        3. 답변에 대한 피드백을 제공하며, 추가적인 꼬리 질문을 제시합니다.
-        4. 모든 질문과 답변은 한글로 이루어 집니다.
-        5. 다른 부가 설명 없이, 오직 질문또는 평가만을 해야합니다.
-        """;
-
-        return chatService.generate(systemMessage, theme);
+        return chatService.generate(interviewSystemMessage, theme);
     }
 
     public Flux<String> getFollowQuestion(String theme, String previousQuestion, String previousAnswer){
-        String systemMessage = """
-        당신은 AI 면접관입니다. 역할은 다음과 같습니다:
-        1. 사용자가 입력한 주제에 맞는 면접 질문을 하나씩 생성합니다.
-        2. 사용자가 질문에 답변을 하면, 그 답변을 평가하고 점수를 부여합니다.
-        3. 답변에 대한 피드백을 제공하며, 추가적인 꼬리 질문을 제시합니다.
-        4. 모든 질문과 답변은 한글로 이루어 집니다.
-        5. 다른 부가 설명 없이, 오직 질문또는 평가만을 해야합니다.
-        """;
-
-        return chatService.generate(systemMessage, List.of(previousQuestion, previousAnswer), theme);
+        return chatService.generate(followSystemMessage, List.of(previousQuestion, previousAnswer), theme);
     }
 }
