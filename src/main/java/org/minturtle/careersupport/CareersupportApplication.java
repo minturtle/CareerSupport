a6cda63c2b4e512f3172c5e338d9e06f14b7aced
minturtle <minseok0425@kakao.com>
2024-09-15 18:07:08 +0900

    feat: initial Commit

---
+++ src/main/java/org/minturtle/careersupport/CareersupportApplication.java
@@ -0,0 +1,13 @@
+package org.minturtle.careersupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareersupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareersupportApplication.class, args);
+	}
+
+}
