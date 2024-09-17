647e1a31f2e57da56b16fcba65896e86218a279b
minturtle <minseok0425@kakao.com>
2024-09-17 15:19:30 +0900

    feat: Cors 설정

--- src/main/java/org/minturtle/careersupport/common/config/SecurityConfig.java
+++ src/main/java/org/minturtle/careersupport/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.minturtle.careersupport.common.config;
 
 
 import org.minturtle.careersupport.auth.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
@@ -9,6 +10,12 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -16,6 +23,11 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    @Value("${cors.origins}")
+    private List<String> origins;
+
 
     @Bean
     public SecurityWebFilterChain securityFilterChain(
@@ -28,6 +40,17 @@ public class SecurityConfig {
                         .pathMatchers("/api/users/register", "/api/users/login").permitAll()
                         .anyExchange().authenticated()
                 )
+                .cors(corsSpec -> corsSpec.configurationSource(
+                        req-> {
+                            CorsConfiguration config = new CorsConfiguration();
+                            config.setAllowedOrigins(Arrays.asList(origins.toArray(new String[0])));
+                            config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                            config.setAllowedHeaders(Collections.singletonList("*"));
+                            config.setAllowCredentials(true);
+                            config.setMaxAge(3600L);
+                            return config;
+                        }
+                ))
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
