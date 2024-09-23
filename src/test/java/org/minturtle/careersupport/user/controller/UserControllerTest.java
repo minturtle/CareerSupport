91b97174b37bda4d5b6b2f7c108edd13a59662d3
minturtle <minseok0425@kakao.com>
2024-09-23 23:46:11 +0900

    test: User 로그인 테스트 코드 작성

--- src/test/java/org/minturtle/careersupport/user/controller/UserControllerTest.java
+++ src/test/java/org/minturtle/careersupport/user/controller/UserControllerTest.java
@@ -3,14 +3,22 @@ package org.minturtle.careersupport.user.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.minturtle.careersupport.testutils.IntegrationTest;
 import org.minturtle.careersupport.user.dto.UserInfoDto;
+import org.minturtle.careersupport.user.dto.UserLoginRequest;
+import org.minturtle.careersupport.user.dto.UserLoginResponse;
 import org.minturtle.careersupport.user.dto.UserRegistrationRequest;
 import org.minturtle.careersupport.user.entity.User;
 import org.minturtle.careersupport.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 class UserControllerTest extends IntegrationTest {
@@ -18,15 +26,19 @@ class UserControllerTest extends IntegrationTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     @DisplayName("사용자는 아직 회원가입 되지 않은 Username과 Password로 회원가입을 할 수 있다.")
-    void registerUser_Success() {
+    void registerUserSuccess() {
+        // given
         String nickname = "nickname";
         String username = "username";
         String password = "password";
         UserRegistrationRequest request = new UserRegistrationRequest(nickname, username, password);
 
-
+        // when
         UserInfoDto actual = webTestClient.post()
                 .uri("/api/users/register")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -37,6 +49,7 @@ class UserControllerTest extends IntegrationTest {
                 .returnResult()
                 .getResponseBody();
 
+        // then
         assertThat(actual.getId()).isNotNull();
         assertThat(actual.getUsername()).isEqualTo(username);
         assertThat(actual.getNickname()).isEqualTo(nickname);
@@ -45,8 +58,9 @@ class UserControllerTest extends IntegrationTest {
 
 
     @Test
-    @DisplayName("사용자는 이미 회원가입 된 username을 입력할 시 409 코드를 반환한다.")
-    void registerUser_Duplication() {
+    @DisplayName("사용자는 이미 회원가입 된 username을 입력할 시 409 코드를 반환받는다.")
+    void registerUserDuplication() {
+        //given
         String nickname = "nickname";
         String username = "username";
         String password = "password";
@@ -55,6 +69,7 @@ class UserControllerTest extends IntegrationTest {
         userRepository.save(new User("123", "nick", username, password)).block();
         UserRegistrationRequest request = new UserRegistrationRequest(nickname, username, password);
 
+        //when & then
         webTestClient.post()
                 .uri("/api/users/register")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -63,8 +78,67 @@ class UserControllerTest extends IntegrationTest {
                 .expectStatus().isEqualTo(HttpStatus.CONFLICT);
     }
 
+    @Test
+    @DisplayName("회원가입이 완료된 사용자는 로그인을 수행해 JWT 토큰을 반환받을 수 있다.")
+    public void loginUserSuccess() throws Exception{
+        //given
+        String nickname = "nickname";
+        String username = "username";
+        String password = "password";
+
+        userRepository.save(new User("123", nickname, username, passwordEncoder.encode(password))).block();
+
+        //when
+        UserLoginRequest request = new UserLoginRequest(username, password);
+        UserLoginResponse actual = webTestClient.post()
+                .uri("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UserLoginResponse.class)
+                .returnResult()
+                .getResponseBody();
+        //then
+        assertThat(actual.getNickname()).isEqualTo(nickname);
+        assertThat(actual.getToken()).isNotNull();
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("getLoginTestArguments")
+    @DisplayName("잘못된 Username 또는 Password를 입력할 시 401 코드를 반환받는다")
+    public void loginInvalidUsername(
+            String correctUsername,
+            String correntPassword,
+            String inputUsername,
+            String inputPassword
+    ) throws Exception{
+        //given
+        userRepository.save(new User("123", "nickname", correctUsername, passwordEncoder.encode(correntPassword))).block();
+
+        //when & then
+        UserLoginRequest request = new UserLoginRequest(inputUsername, inputPassword);
+        webTestClient.post()
+                .uri("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isUnauthorized();
+
+    }
+
     @BeforeEach
     void setUp() {
         userRepository.deleteAll().block();
+    }
+
+
+    protected static Stream<Arguments> getLoginTestArguments(){
+        return Stream.of(
+                Arguments.of("username", "password", "user", "password"),
+                Arguments.of("username", "password", "username", "pwd"),
+                Arguments.of("username", "password", "user", "pwd")
+        );
     }
 }
