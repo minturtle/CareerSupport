e64e4c812ef636ac47d6b62c51ac23c1891c9f0a
minturtle <minseok0425@kakao.com>
2024-10-14 10:33:24 +0900

    refactor: CodeReview Controller 및 Interface 재작성

--- src/main/java/org/minturtle/careersupport/codereview/service/CodeReviewService.java
+++ src/main/java/org/minturtle/careersupport/codereview/service/CodeReviewService.java
@@ -1,8 +1,9 @@
 package org.minturtle.careersupport.codereview.service;
 
+import org.minturtle.careersupport.codereview.dto.CodeReviewRequest;
 import reactor.core.publisher.Mono;
 
 public interface CodeReviewService {
 
-    Mono<Void> doCodeReview(String repositoryName, Long prNumber, String fileName, String fileContent);
+    Mono<Void> doCodeReview(CodeReviewRequest codeReviewRequest);
 }
