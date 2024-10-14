adb50160d9b8ecd9f0171605cae05d45876ebce9
minturtle <minseok0425@kakao.com>
2024-10-14 11:52:26 +0900

    feat: AI 코드리뷰 구현체 구현

---
+++ src/main/java/org/minturtle/careersupport/codereview/service/AiCodeReviewClientImpl.java
@@ -0,0 +1,41 @@
+package org.minturtle.careersupport.codereview.service;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.minturtle.careersupport.codereview.dto.PullRequestFile;
+import org.minturtle.careersupport.common.exception.InternalServerException;
+import org.minturtle.careersupport.common.service.ChatService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class AiCodeReviewClientImpl implements AiCodeReviewClient{
+
+    private final ChatService chatService;
+    private final ObjectMapper objectMapper;
+
+    @Value("${spring.ai.openai.messages,code-review-system-message}")
+    private String codeReviewSystemMessage;
+
+    @Override
+    public Flux<ReviewResponse> getAiCodeReview(List<ReviewRequest> files) {
+        return Flux.fromIterable(files).flatMap(
+                file-> {
+                    try {
+                        return chatService.generate(codeReviewSystemMessage, objectMapper.writeValueAsString(file))
+                                .collect(Collectors.joining())
+                                .map(result -> new ReviewResponse(file.getFileName(), result));
+                    } catch (JsonProcessingException e) {
+                        throw new InternalServerException("댓글 생성 중 오류가 발생했습니다.");
+                    }
+                }
+        );
+    }
+}
