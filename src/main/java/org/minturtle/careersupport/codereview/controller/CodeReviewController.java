796eb7e47c1906e951b15a8de80b35882ffcb004
YeaChan05 <qkenrdl05@gmail.com>
2024-10-15 20:44:59 +0900

    fix: 엔드포인트를 간소화

--- src/main/java/org/minturtle/careersupport/codereview/controller/CodeReviewController.java
+++ src/main/java/org/minturtle/careersupport/codereview/controller/CodeReviewController.java
@@ -7,37 +7,17 @@ import org.minturtle.careersupport.codereview.service.CodeReviewService;
 import org.minturtle.careersupport.common.utils.Base64Utils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/code-review")
 @RequiredArgsConstructor
 public class CodeReviewController {
-
-    private final Base64Utils base64Utils;
     private final CodeReviewService codeReviewService;
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.OK)
-    public Mono<Void> doCodeReview(@RequestBody CodeReviewRequest codeReviewRequest){
-        return Flux.fromIterable(codeReviewRequest.getFileContents().entrySet())
-                .flatMap(entry -> {
-                    String key = entry.getKey();
-                    String encodedValue = entry.getValue();
-
-                    return Mono.just(encodedValue)
-                            .map(base64Utils::decode)
-                            .flatMap(decodedValue ->
-                                codeReviewService.doCodeReview(
-                                        codeReviewRequest.getRepositoryName(),
-                                        codeReviewRequest.getPrNumber(),
-                                        key,
-                                        decodedValue
-                                )
-                            );
-                })
-                .then();
-
+    public Mono<Void> doCodeReview(@RequestBody CodeReviewRequest codeReviewRequest) {
+        return codeReviewService.doCodeReview(codeReviewRequest);
     }
 }
