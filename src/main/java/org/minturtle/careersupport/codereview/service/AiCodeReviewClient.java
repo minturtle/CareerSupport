3a6fd0ae76a9c2626ab57a6149e8f045abcad5ae
YeaChan05 <qkenrdl05@gmail.com>
2024-10-15 20:44:45 +0900

    fix: 인터페이스 시그니처를 서비스 로직에 따라 수정

--- src/main/java/org/minturtle/careersupport/codereview/service/AiCodeReviewClient.java
+++ src/main/java/org/minturtle/careersupport/codereview/service/AiCodeReviewClient.java
@@ -1,8 +1,11 @@
 package org.minturtle.careersupport.codereview.service;
 
-import reactor.core.publisher.Mono;
+import java.util.List;
+import org.minturtle.careersupport.codereview.dto.github.request.ReviewComment;
+import org.minturtle.careersupport.codereview.dto.github.response.PullRequestFile;
+import reactor.core.publisher.Flux;
 
 public interface AiCodeReviewClient {
 
-    Mono<String> getAiCodeReview(String fileName, String fileContent);
+    Flux<ReviewComment> getAiCodeReview(List<PullRequestFile> files);
 }
