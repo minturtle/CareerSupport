53b5fa6a94c7386ad80d2658d2700303ffab11d3
YeaChan05 <qkenrdl05@gmail.com>
2024-10-16 11:04:24 +0900

    refactor: 로직 처리방식 변경

--- src/main/java/org/minturtle/careersupport/codereview/service/GithubCodeReviewService.java
+++ src/main/java/org/minturtle/careersupport/codereview/service/GithubCodeReviewService.java
@@ -1,7 +1,6 @@
 package org.minturtle.careersupport.codereview.service;
 
 import java.io.IOException;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kohsuke.github.GHPullRequest;
@@ -28,52 +27,34 @@ public class GithubCodeReviewService implements CodeReviewService {
         String token = codeReviewRequest.getGithubToken();
         String repositoryName = codeReviewRequest.getRepositoryName();
         Long prNumber = codeReviewRequest.getPrNumber();
-        return Mono.fromCallable(() -> {
-                    GHPullRequest pullRequest = getPullRequest(token, repositoryName, prNumber);
-                    return pullRequest.listFiles();
-                })
-                .publishOn(Schedulers.boundedElastic())
-                .flatMapMany(files -> {
-                    try {
-                        return Flux.fromIterable(files.toList().stream()
-                                        .map(ReviewRequest::from)
-                                        .toList());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .collectList()
-                .flatMap(files -> codeReviewClient.getAiCodeReview(files)
-                        .collectList()
-                        .flatMap(reviewComments -> postCommentsToPullRequest(
-                                token,
-                                repositoryName,
-                                prNumber,
-                                reviewComments)))
+        return getPullRequest(token, repositoryName, prNumber)
+                .subscribeOn(Schedulers.boundedElastic())
+                .flatMap(pullRequest -> Flux.fromIterable(pullRequest.listFiles())
+                        .map(ReviewRequest::from)
+                        .transform(codeReviewClient::getAiCodeReview)
+                        .flatMap(reviewComment -> postCommentsToPullRequest(
+                                pullRequest,
+                                reviewComment)).then()
+                );
+    }
+
+    private Mono<Void> postCommentsToPullRequest(GHPullRequest pullRequest, ReviewResponse reviewComment) {
+        return Mono.fromCallable(() ->
+                        pullRequest.createReview()
+                                .singleLineComment(reviewComment.getReviewContent(), reviewComment.getFileName(), 1)
+                                .event(GHPullRequestReviewEvent.COMMENT)
+                                .create())
+                .subscribeOn(Schedulers.boundedElastic())
                 .then();
     }
 
-
-    private Mono<Void> postCommentsToPullRequest(String token, String repositoryName, Long prNumber,
-                                                 List<ReviewResponse> reviewComments) {
-        return Mono.fromCallable(() -> getPullRequest(token, repositoryName, prNumber))
-                .publishOn(Schedulers.boundedElastic())
-                .flatMapMany(pullRequest -> Flux.fromIterable(reviewComments)
-                        .flatMap(comment -> Mono.fromCallable(
-                                        () -> pullRequest.createReview()
-                                                .singleLineComment(comment.getReviewContent(), comment.getFileName(), 1)
-                                                .event(GHPullRequestReviewEvent.COMMENT)
-                                                .create()
-                                        )
-                                        .doOnError(Throwable::printStackTrace)
-                        ))
-                .then();
-    }
-
-    private static GHPullRequest getPullRequest(String token, String repositoryName, Long prNumber)
-            throws IOException {
-        GitHub github = new GitHubBuilder().withOAuthToken(token).build();
-        GHRepository repository = github.getRepository(repositoryName);
-        return repository.getPullRequest(Math.toIntExact(prNumber));
+    private static Mono<GHPullRequest> getPullRequest(String token, String repositoryName, Long prNumber) {
+        try {
+            GitHub github = new GitHubBuilder().withOAuthToken(token).build();
+            GHRepository repository = github.getRepository(repositoryName);
+            return Mono.just(repository.getPullRequest(Math.toIntExact(prNumber)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
