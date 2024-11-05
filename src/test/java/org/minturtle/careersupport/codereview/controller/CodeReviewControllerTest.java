8d6b1f9e8b2ceb00c9b1d0101d4d0090498a04f8
YeaChan05 <qkenrdl05@gmail.com>
2024-11-05 19:28:56 +0900

    test: 코드 변경에 따른 테스트 수정

--- src/test/java/org/minturtle/careersupport/codereview/controller/CodeReviewControllerTest.java
+++ src/test/java/org/minturtle/careersupport/codereview/controller/CodeReviewControllerTest.java
@@ -3,19 +3,25 @@ package org.minturtle.careersupport.codereview.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.kohsuke.github.*;
+import org.kohsuke.github.GHCommit.File;
 import org.minturtle.careersupport.codereview.dto.CodeReviewRequest;
+import org.minturtle.careersupport.codereview.entity.CommitPinpoint;
+import org.minturtle.careersupport.codereview.respository.ReviewPinpointRepository;
 import org.minturtle.careersupport.codereview.service.AiCodeReviewClient;
+import org.minturtle.careersupport.codereview.service.AiCodeReviewClient.ReviewResponse;
 import org.minturtle.careersupport.common.dto.CommonResponseBody;
 import org.minturtle.careersupport.common.facade.GithubPullRequestFacade;
 import org.minturtle.careersupport.testutils.IntegrationTest;
 import org.minturtle.careersupport.user.dto.UserInfoDto;
 import org.minturtle.careersupport.user.entity.User;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 
 
 import java.util.List;
+import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
@@ -23,7 +29,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 class CodeReviewControllerTest extends IntegrationTest {
-
+    @MockBean
+    ReviewPinpointRepository reviewPinpointRepository;
 
     @Test
     @DisplayName("Code Review에 필요한 Request Body를 코드리뷰를 요청할 수 있다.")
@@ -35,29 +42,36 @@ class CodeReviewControllerTest extends IntegrationTest {
         String repositoryName = "minturtle/careersupport";
         int prNumber = 1;
 
-        List<GHPullRequestFileDetail> fakeGithubReqFileDetail = List.of(
-                 mock(GHPullRequestFileDetail.class)
-        );
+        File[] files = new File[]{mock(File.class)};
+        Flux<File> fakeCommitsDiff = Flux.fromArray(files);
 
         GithubPullRequestFacade mockPrFacade = mock(GithubPullRequestFacade.class);
+        CommitPinpoint pinpoint = CommitPinpoint.builder()
+                .prNumber(prNumber)
+                .sha("sha")
+                .build();
+
 
         given(githubUtils.generatePullRequest(ghpToken, repositoryName, prNumber))
                 .willReturn(mockPrFacade);
-        given(mockPrFacade.getChangedFiles()).willReturn(fakeGithubReqFileDetail);
+        given((mockPrFacade.getCommitsDiff()))
+                .willReturn(fakeCommitsDiff);
 
-        given(fakeGithubReqFileDetail.get(0).getFilename()).willReturn("test.java");
-        given(fakeGithubReqFileDetail.get(0).getStatus()).willReturn("modified");
-        given(fakeGithubReqFileDetail.get(0).getPatch()).willReturn("122");
+        given(files[0].getSha()).willReturn("sha");
+        given(files[0].getFileName()).willReturn("test.java");
+        given(files[0].getStatus()).willReturn("modified");
+        given(files[0].getPatch()).willReturn("122");
+
+        given(reviewPinpointRepository.findByPrNumber(anyInt())).willReturn(Mono.empty());
+        given(reviewPinpointRepository.save(any())).willReturn(Mono.just(pinpoint));
 
         given(chatService.generate(any(), any())).willReturn(Flux.just("AI", "댓글", "테스트"));
-
 
         CodeReviewRequest codeReviewRequestBody = CodeReviewRequest.builder()
                 .repositoryName(repositoryName)
                 .prNumber(prNumber)
                 .githubToken(ghpToken)
                 .build();
-
 
         // when & then
         CommonResponseBody<List<AiCodeReviewClient.ReviewResponse>> responseBody = webTestClient.post()
@@ -72,13 +86,9 @@ class CodeReviewControllerTest extends IntegrationTest {
                 .returnResult()
                 .getResponseBody();
 
-        verify(mockPrFacade, times(1)).getChangedFiles();
+        verify(mockPrFacade, times(1)).getCommitsDiff();
         assertThat(responseBody.getData()).hasSize(1);
         assertThat(responseBody.getData().get(0)).extracting("fileName", "reviewContent")
                 .containsExactly("test.java", "AI댓글테스트");
     }
-
-
-
-
 }
