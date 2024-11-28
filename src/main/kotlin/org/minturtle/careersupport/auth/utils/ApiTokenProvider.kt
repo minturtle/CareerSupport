f60ed046efaffa43089c913642b2a895501ee511
YeaChan05 <qkenrdl05@gmail.com>
2024-11-28 16:52:36 +0900

    fix: 복호화 예외 발생시 처리 구문 추가

--- src/main/kotlin/org/minturtle/careersupport/auth/utils/ApiTokenProvider.kt
+++ src/main/kotlin/org/minturtle/careersupport/auth/utils/ApiTokenProvider.kt
@@ -8,6 +8,7 @@ import org.minturtle.careersupport.common.exception.InternalServerException
 import org.minturtle.careersupport.common.utils.EncryptUtils
 import org.minturtle.careersupport.user.dto.UserInfoDto
 import org.springframework.stereotype.Component
+import java.nio.BufferUnderflowException
 
 
 @Logging
@@ -15,7 +16,7 @@ import org.springframework.stereotype.Component
 class ApiTokenProvider(
     private val encryptUtils: EncryptUtils,
     private val objectMapper: ObjectMapper
-){
+) {
 
     fun generate(userInfoDto: UserInfoDto): String {
         return try {
@@ -26,18 +27,18 @@ class ApiTokenProvider(
     }
 
     fun decryptApiToken(token: String): UserInfoDto {
-         if (!token.startsWith("cs_")) {
+        if (!token.startsWith("cs_")) {
             throw BadRequestException("토큰이 올바르지 않습니다.")
         }
 
-        val decrypted = encryptUtils.decrypt(token.substring(3))
-
         return runCatching {
+            val decrypted = encryptUtils.decrypt(token.substring(3))
             objectMapper.readValue(decrypted, UserInfoDto::class.java)
-        }.onFailure {
-            throw InternalServerException("API 토큰 해독 중 예상치 못한 오류가 발생했습니다.")
+        }.onFailure { e ->
+            when (e) {
+                is BufferUnderflowException -> throw BadRequestException("복호화에 실패했습니다.")
+                else -> throw InternalServerException("API 토큰 해독 중 예상치 못한 오류가 발생했습니다.")
+            }
         }.getOrThrow()
-
     }
-
 }
